74c668dd6324a57c151697137e86465454599ca2
Alan Wen <alanjameswen@outlook.com>
2022-04-19 00:15:29 -0700

    Static Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,62 @@
 import '../styles/Home.scss';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function Home() {
+    const onClickJoin = () => {
+        console.log('join');
+    };
+
+    const onClickCreate = () => {
+        console.log('create');
+    };
+
     return (
-        <p>Home</p>
+        <div className='page-container'>
+            <main>
+                <Stack
+                    direction='column'
+                    spacing={2}
+                >
+                    <h1 id='title'>JUDGEMENT CALL</h1>
+                    <TextField 
+                        fullWidth
+                        className='text-field'
+                        id="input-name" 
+                        label="Name" 
+                        variant="outlined" 
+                        margin="normal"
+                    />
+                    <TextField 
+                        fullWidth
+                        className='text-field'
+                        id="input-code" 
+                        label="Room Code (Optional)" 
+                        variant="outlined" 
+                        margin="normal"
+                    />
+                    <Stack 
+                        spacing={2}
+                        direction='row'
+                        justifyContent='center'
+                    >
+                        <Button 
+                            variant="contained"
+                            color="secondary"
+                            onClick={onClickCreate}>
+                            New Game
+                        </Button>
+                        <Button 
+                            variant="outlined"
+                            color="secondary"
+                            onClick={onClickJoin}>
+                            Join Game
+                        </Button>
+                    </Stack>
+                </Stack>
+            </main>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './components/App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    secondary: {
+      main: '#A6924D',
+    }
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
